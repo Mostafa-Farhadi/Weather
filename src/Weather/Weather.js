ea57110d1c185117327709e4892fb77028b761d0
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-02-16 21:34:25 +0330

    update design

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -1,65 +1,15 @@
 import React, {useState} from 'react';
 
-// material ui core components
-import { Grid, Paper, Typography, TextField, makeStyles } from '@material-ui/core';
-
 // icons
-import clouds from '../assets/img/icons/clouds.png';
-import clear from '../assets/img/icons/clear.png';
-import snow from '../assets/img/icons/snow.png';
-import rain from '../assets/img/icons/rain.png';
-import haze from '../assets/img/icons/haze.png';
-import mist from '../assets/img/icons/mist.png';
-import fog from '../assets/img/icons/fog.png';
+import clouds from '../assets/img/clouds.png';
+import clear from '../assets/img/clear.png';
+import snow from '../assets/img/snow.png';
+import rain from '../assets/img/rain.png';
+import haze from '../assets/img/haze.png';
+import mist from '../assets/img/mist.png';
+import fog from '../assets/img/fog.png';
 
-
-
-const useStyles = makeStyles ((theme) =>({
-    root: {
-        marginTop: 80,
-    },
-    default: {
-        backgroundColor: '#40739e',
-    },
-    clear: {
-        backgroundColor: '#f1c40f',
-    },
-    clouds: {
-        backgroundColor: '#273c75',
-    },
-    rain: {
-        backgroundColor: '#16a085',
-    },
-    snow: {
-        backgroundColor: '#f5f6fa',
-    },
-    haze: {
-        backgroundColor: '#95a5a6',
-    },
-    mist: {
-        backgroundColor: '#8e44ad',
-    },
-    input: {
-        width: 300,
-        marginTop: 10,
-    },
-    city: {
-        marginTop: 50,
-    },
-    date: {
-        marginTop: 40,
-    },
-    temperature: {
-        marginTop: 50,
-    },
-    weather: {
-        marginTop: 50,
-    },
-    weatheIcon: {
-        marginLeft: 10,
-        marginTop: 55,
-    },
-}));
+import '../assets/css/style.css';
 
 const api = {
     key: '26eb107ac36fa5a8381d27d5206ad752',
@@ -67,10 +17,8 @@ const api = {
 };
 
 function Weather() {
-    const classes = useStyles();
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
-    const [background, setBackground] = useState(classes.default);
     const [icon, setIcon] = useState('')
 
     
@@ -86,29 +34,21 @@ function Weather() {
                 }
                 if (data.weather[0].main === 'Clouds') {
                     setIcon(clouds);
-                    setBackground(classes.clouds);
                 }else if (data.weather[0].main === 'Clear') {
                     setIcon(clear);
-                    setBackground(classes.clear);
                 }else if (data.weather[0].main === 'Snow') {
                     setIcon(snow);
-                    setBackground(classes.snow);
                 }else if (data.weather[0].main === 'Rain') {
                     setIcon(rain);
-                    setBackground(classes.rain);
                 }else if (data.weather[0].main === 'Haze') {
                     setIcon(haze);
-                    setBackground(classes.haze);
                 }else if (data.weather[0].main === 'Mist') {
                     setIcon(mist);
-                    setBackground(classes.mist);
                 }else if (data.weather[0].main === 'Fog') {
                     setIcon(fog);
-                    setBackground(classes.mist);
                 }
                 else {
                     setIcon('');
-                    setBackground(classes.default);
                 }
             })
             .catch((err) => {
@@ -129,48 +69,34 @@ function Weather() {
     };
 
     return (
-        <Grid container justify="center" className={classes.root}>
-            <Paper elevation={10} className={background} style={{width: 400, height: 600, color: '#e84118',}}>
-                <Grid container justify="center">
-                    <TextField 
-                        label="City name..."
-                        value={query}
-                        color="secondary"
-                        onChange={(event) => setQuery(event.target.value)} 
-                        onKeyPress={searchPress} 
-                        autoFocus
-                        variant="standard" 
-                        className={classes.input} />
-                </Grid>
-                {(typeof weather.main != "undefined") ? (
-                <Grid>
-                    <Grid container justify="center" alignItems="center">
-                        <Typography paragraph variant="h4" className={classes.city}>
-                            {weather.name}, {weather.sys.country}
-                        </Typography>
-                    </Grid>
-                    <Grid container justify="center" alignItems="center">
-                        <Typography paragraph variant="h6" className={classes.date}>
-                            {dateBuilder(new Date())}
-                        </Typography>
-                    </Grid>
-                    <Grid container direction="column" justify="center" alignItems="center">
-                        <Typography paragraph variant="h2" className={classes.temperature}>
-                            {Math.round(weather.main.temp)}°C {/*Alt 0176*/}
-                        </Typography>
-                    </Grid>
-                    <Grid container direction="row" justify="center" alignItems="center">
-                        <Typography paragraph variant="h5" className={classes.weather}>
-                            {weather.weather[0].main}
-                        </Typography>
-                        <Typography paragraph variant="h5" className={classes.weatheIcon}>
-                            <img src={icon} alt="weather"/>
-                        </Typography>
-                    </Grid>
-                </Grid>
-                ) : ('')}
-            </Paper>
-        </Grid>
+        <div className="root">
+            <input 
+                type="text" 
+                placeholder="City..." 
+                value={query} 
+                onChange={(event) => setQuery(event.target.value)}
+                onKeyPress={searchPress}
+                className="input"
+                autoFocus
+            />
+            {typeof weather.main != "undefined" ? (
+                <>
+                    <p className="city">
+                        {weather.name}, {weather.sys.country}
+                    </p>
+                    <p className="date">
+                        {dateBuilder(new Date())}
+                    </p>
+                    <p className="temprature">
+                        {Math.round(weather.main.temp)}°C {/*Alt 0176*/}
+                    </p>
+                    <div className="weather">
+                        <p>{weather.weather[0].main}</p>
+                        <img src={icon} alt="weather" className="weather-icon"/>
+                    </div>
+                </>
+            ) : (' ')}
+        </div>
     )
 }
 
