4d2cdd7949c509cf6d41287e04930652cf48d9c9
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-07 00:37:22 +0330

    New updates...
    1. Change background of root div of Weather component and change font of entire app.
    
    2. Use spreed operator for passing props from Daily to Days Component.
    
    3. Remove humidity and wind spead data on daily imformations.
    
    4. Add sunrise and sunset informations and also add two images for them.
    
    5. Edit css file for these changes.

--- src/components/Daily/Days.js
+++ src/components/Daily/Days.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import sun from './sun.png'
+import moon from './moon.png'
 
 function Days(props) {
-    const {weekDay, maxTemp, minTemp, humidity, windSpeed, iconcode} = props
+    const {weekDay, maxTemp, minTemp, sunrise, sunset, timezoneOffset, iconcode} = props
     const futureDays = (millisecond) => {
         const date = new Date(millisecond * 1000)
         let dayNumber = date.getDay()
@@ -9,6 +11,14 @@ function Days(props) {
         let day = days[dayNumber].slice(0,3)
         return `${day}`
     }
+
+    const sunriseDate = new Date((sunrise + timezoneOffset) * 1000)
+    const sunriseHour = sunriseDate.getUTCHours()
+    const seuriseMinute = sunriseDate.getUTCMinutes()
+
+    const sunsetDate = new Date((sunset + timezoneOffset) * 1000)
+    const sunsetHour = sunsetDate.getUTCHours()
+    const seuserMinute = sunsetDate.getUTCMinutes()
 
     return (
         <div className="daily-container">
@@ -18,8 +28,14 @@ function Days(props) {
                 <p className="min-temperature">{Math.round(minTemp)}°C</p>
             </div>
             <img className="weather-icon" src={`http://openweathermap.org/img/w/${iconcode}.png`} alt="icon"/>
-            <p className="humidity">{humidity}%</p>
-            <p className="wind-speed">{windSpeed}m/s</p>
+            <div className="sunrise-container">
+                <img src={sun} alt="sun" className="sun-moon-icon" />
+                <p>{sunriseHour}:{seuriseMinute}</p>
+            </div>
+            <div className="sunset-container">
+                <img src={moon} alt="moon" className="sun-moon-icon" />
+                <p>{sunsetHour}:{seuserMinute}</p>
+            </div>
         </div>
     )
 }
