fb8ada394d2bc3f17728534eae7100365ae2b14b
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-03-07 09:55:53 +0330

    fix bug of link of moon.png

--- src/components/Daily/Days.js
+++ src/components/Daily/Days.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import sun from '../../assets/img/sun.png'
-import moon from '../../assets/img/sun.png'
+import moon from '../../assets/img/moon.png'
 
 function Days(props) {
     const {weekDay, maxTemp, minTemp, sunrise, sunset, timezoneOffset, iconcode} = props
