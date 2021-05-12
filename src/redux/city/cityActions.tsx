93555e65914f83901d8869a14e0972652ff1170d
Mostafa Farhadi <mostafafarhadi74@yahoo.com>
2021-05-12 14:09:41 +0430

    Use async await feature to handle multiple data fetching

--- src/redux/city/cityActions.tsx
+++ src/redux/city/cityActions.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 import {FETCH_CITY_REQUEST, FETCH_CITY_SUCCESS, FETCH_CITY_FAILURE} from './cityTypes';
 
 const fetchCityRequest = () => {
@@ -23,25 +23,19 @@ const fetchCityFailure = (error: string) => {
     };
 };
 
-export const fetchCity = (cityName: string) => {
-    return (dispatch: Function) => {
-        dispatch(fetchCityRequest);
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=26eb107ac36fa5a8381d27d5206ad752`)
-        .then(response => {    
-            const onlineData = response.data;
-            axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${onlineData.coord.lat}&lon=${onlineData.coord.lon}&units=metric&exclude=minutely,alerts&appid=8277afc975ef94a282c1e6077eb4320c`)
-            .then(response => {
-                const dailyData = response.data;
-                axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${dailyData.lat}&lon=${dailyData.lon}&units=metric&cnt=16&appid=26eb107ac36fa5a8381d27d5206ad752`)
-                .then(response => {
-                    const hourlyData = response.data;
-                    dispatch(fetchCitySeuccess(onlineData, dailyData, hourlyData));
-                })
-            })
-        })
-        .catch(error => {
-            const errorMsg = error.message
-            dispatch(fetchCityFailure(errorMsg))
-        });
+export const fetchCity = (cityName: string) => async (dispatch: any) => {
+    dispatch(fetchCityRequest);
+    try {
+        const onlineDataResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=26eb107ac36fa5a8381d27d5206ad752`);
+        const onlineData = await onlineDataResponse.json();
+        const dailyDataResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${onlineData.coord.lat}&lon=${onlineData.coord.lon}&units=metric&exclude=minutely,alerts&appid=8277afc975ef94a282c1e6077eb4320c`);
+        const dailyData = await dailyDataResponse.json();
+        const hourlyDataResponse = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${dailyData.lat}&lon=${dailyData.lon}&units=metric&cnt=16&appid=26eb107ac36fa5a8381d27d5206ad752`);
+        const hourlyData = await hourlyDataResponse.json();
+        dispatch(fetchCitySeuccess(onlineData, dailyData, hourlyData));
+    }
+    catch(error) {
+        const errorMsg = error.message
+        dispatch(fetchCityFailure(errorMsg))
     };
 };
